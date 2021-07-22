a8bfeeb1c53d7b5c1e7a53561d5360632a4c8032
PakJeon <maibairun@163.com>
2021-07-22 16:13:38 +0800

    fix: 修复call

--- deepJs/call.js
+++ deepJs/call.js
@@ -1,11 +1,11 @@
 /*
  * :file description: 
- * :name: /js-handwriting/deepJs/call.js
+ * :name: \js-handwriting\deepJs\call.js
  * :author: PakJeon
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-07-15 23:06:46
  * :last editor: PakJeon
- * :date last edited: 2021-07-15 23:19:58
+ * :date last edited: 2021-07-22 16:12:53
  */
 Function.prototype.myCall = function (context) {
 	const tmpContext = context || window;  // this参数可能传null，当为null时指向window
@@ -14,7 +14,7 @@ Function.prototype.myCall = function (context) {
 	for (let i = 1; i < arguments.length; i++) {  // 把参数都放在数组中
 		args.push('arguments[' + i + ']');
 	}
-	const result = eval('context.fn(' + args + ')');  // 数组隐式转字符串
+	const result = eval('tmpContext.fn(' + args + ')');  // 数组隐式转字符串
 	delete tmpContext.fn  // 把fn属性删除
 	return result;
 }
